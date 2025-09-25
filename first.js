56bc3da0e4ee7bfb424d40147b025ed630aeccad
bharathn10 <bharathnnagrajuk@gmail.com>
2025-09-25 15:51:01 +0530

    new.commit

--- first.js
+++ first.js
@@ -1,2 +1,166 @@
-name = "osijfi";
-console.log(name);
+// {
+//     let a ;
+//     console.log(a);
+// }
+
+// const { Profiler } = require("react")
+
+// a = 20;
+// a=true;
+// {
+//     let b  = null;
+    
+// }
+// b = null;
+
+// c= BigInt("1234");
+
+// d = Symbol("hello!");
+
+// const e ={
+//     name : 'john',
+//     age : 22,
+//     pass: true,
+//     cgpa:9.5
+// }
+// console.log(e);
+// console.log(e['name']);
+// console.log(e['cgpa'])
+
+// const product = {
+//      name : 'Pen',
+//     price :  10,
+//     ratings : 5.5,
+//     offer : "5%"
+// }
+   
+// console.log(product);
+
+// const profil = {
+//     name : "rahul",
+//     follwers : 200,
+//     followings : 290,
+//     Dicription : "hello my name is rhaul"
+// }
+// console.log(profil);
+// console.log(typeof profil['name'])
+
+// let a = 5;
+// let b = 5;
+// console.log("a=",a, "b=",b);
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+
+// let age = 16;
+
+// if (age >= 18){
+//     console.log("your are eligible for vote")
+// }
+// if (age < 18){
+//     console.log("your not eligible for vote")
+// }
+
+// let mode = "light";
+// let color;
+
+// if (mode ==="dark"){
+//     color = "black";
+// }
+// if (mode ==="light"){
+//     color = "white";
+// }
+
+//     console.log(color);
+
+// let age = 16;
+
+// if (age >= 18){
+//     console.log("you are eligible for vote")
+// }
+// else{
+//     console.log("your not eligible for vote")
+// }
+
+// let num = 5;
+
+// if (num % 2 === 0){
+//     console.log(num,"even number");
+// }
+// else{
+//     console.log(num,"odd number");
+// }
+// let age = 21;
+
+// if (age < 18 && age >11){
+//     console.log("your are teenager");
+// }
+// else if (age > 18 && age < 35){;
+// console.log("your are adult");
+// }
+// else{
+//     console.log("your are senior citizen")
+// }
+
+// let age = 18;
+
+// if (age >= 18 && age <=25){
+//     console.log('your are a adult ');
+// }
+// else if(age >=18 && age >=12){
+//     console.log("your a teenager");
+// }
+// else{
+//     console.log('your a kid or senior citizen');
+// }
+// console.log("end of the program")
+
+// let num = 7;
+
+// if (num % 2 === 0){
+//     console.log(num,"even number");
+// }
+// else{
+//     console.log(num,"odd number");
+// }
+
+// let name = prompt("enter your name");
+// console.log("your name is",name)
+// let num = prompt("enter your number");
+
+// if (num % 5 === 0){
+//     console.log(num,'IS DIVISIBLE BY 5')
+// }else{
+//     console.log(num, "is not divisible by 5")
+// // }
+// let marks = prompt("enter your marks");
+// // marks = Number.parseInt(marks);
+
+// // let marks = 90;
+// if (marks >= 90 && marks <= 100){
+//     console.log("A+");
+// }
+// else if (marks >=70 &&  marks <=80){
+//     console.log("B+");
+// }
+// else if (marks >=60 && marks <=69){
+//     console.log("c+");
+// }
+// else if (marks >=50 && marks <=59){
+//     console.log("D+");
+// }
+// else{
+//     console.log("fail");
+// // }
+// for (let num =1; num<=10; num++){
+//     console.log("bharath");
+// }
+// for (let num =1; num<=10; num++){
+//     console.log("hello!")
+// }
+let sum =0;
+
+for (let num =1;  num<=10; num++){
+    sum= sum+num
+}
+console.log("sum=",sum)
